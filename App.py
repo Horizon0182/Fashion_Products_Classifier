669af6f58affb25624bd6a3ff5beeb7b321db0e9
Horizon0182 <148845763+Horizon0182@users.noreply.github.com>
2026-03-26 03:00:02 +0800

    Update App.py

--- App.py
+++ App.py
@@ -231,7 +231,7 @@ with st.sidebar:
 models = load_models()
 device = models["device"]
 
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png", "wemp"])
 
 if uploaded_file is None:
     st.info("Please upload a fashion product image to begin.")
