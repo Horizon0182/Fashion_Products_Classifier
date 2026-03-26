6c87949153f9056ee22215d9ed0c7445adf68216
Horizon0182 <148845763+Horizon0182@users.noreply.github.com>
2026-03-26 21:28:24 +0800

    Update App.py

--- App.py
+++ App.py
@@ -22,7 +22,7 @@ st.set_page_config(
 # =========================
 CLASSIFIER_MODEL_PATH = "Albatrosszzz/Fashion-Product-Classify-Vit"
 CAPTION_MODEL_PATH = "Salesforce/blip-image-captioning-base"
-LLM_MODEL_PATH = "nvidia/Nemotron-Cascade-2-30B-A3B"
+LLM_MODEL_PATH = "Qwen/Qwen3-0.6B"
 
 
 # =========================
