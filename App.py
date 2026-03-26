917923225db0077da4d65d67f4a7736edcddefe8
Horizon0182 <148845763+Horizon0182@users.noreply.github.com>
2026-03-26 20:39:42 +0800

    Update App.py

--- App.py
+++ App.py
@@ -20,7 +20,7 @@ st.set_page_config(
 # =========================
 # Model paths
 # =========================
-CLASSIFIER_MODEL_PATH = "Albatrosszzz/Fashion_Clothes_Image_Classifier"
+CLASSIFIER_MODEL_PATH = "Albatrosszzz/Fashion-Product-Classify-Vit"
 CAPTION_MODEL_PATH = "Salesforce/blip-image-captioning-base"
 LLM_MODEL_PATH = "Qwen/Qwen3-0.6B"
 
